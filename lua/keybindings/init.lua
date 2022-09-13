f062d2e7b2a390f33a797299a5357349ab884fbd
Vipr728 <105462926+Vipr728@users.noreply.github.com>
2022-09-12 21:42:04 -0700

    Added configuration correctly this time

---
+++ lua/keybindings/init.lua
@@ -0,0 +1,4 @@
+local map = vim.api.nvim_set_keymap
+
+map('i', 'jk', '<ESC>', {noremap = true, silent = false})
+map('i', 'kj', '<ESC>', {noremap = true, silent = false}) 
---
+++ lua/lsp/init.lua
@@ -0,0 +1,5 @@
+require("lsp/cmp")
+require("lsp/lsp")
+require("lsp/mason")
+require("lsp/null-ls")
+
